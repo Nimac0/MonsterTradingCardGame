1de5737a2b0a44ca9cfba0cbd563173f9b6e54e5
nimaco <User@DESKTOP-L00R6R1>
2023-09-18 17:37:37 +0200

    basics

---
+++ MonsterTradingCardGame/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+enum Element
+{
+    WATER = 0,
+    FIRE,
+    NORMAL
+}
+
+namespace MonsterTradingCardGame
+{
+    public abstract class Card
+    {
+
+        private Element _cardElement;
+        private string _cardName;
+        private int _cost;
+
+
+    }
+}
---
+++ MonsterTradingCardGame/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterTradingCardGame
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public User(string UserName)
+        {
+            this.UserName = UserName;
+        }
+    }
+}
